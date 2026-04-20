a4859814db37b17a0828a76b47015de0a06aaacb
Prity Singha <prity.singha.144@gmail.com>
2026-04-20 17:06:12 +0530

    a

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
+
 #!/usr/bin/env bash
 
-# install tesseract
 apt-get update
+
+# Required for sklearn & numpy
+apt-get install -y build-essential
+apt-get install -y libatlas-base-dev
+
+# Tesseract for OCR
 apt-get install -y tesseract-ocr
 
+pip install --upgrade pip
 pip install -r requirements.txt
+
